9c49f0cdcea460351f16856e1214252900065653
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-02-18 22:49:11 +0200

    generated test data for prisma

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,13 +1,13 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Container } from "@chakra-ui/react";
 import React from "react";
 import Header from "../components/Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Container maxW={"container.lg"} p={6}>
-          <Header />
-          {children}
+      <Container maxW={"container.xl"} p={6}>
+        <Header />
+        {children}
       </Container>
     </>
   );
