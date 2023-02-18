729324eaffe96688bf76bf6511d9c65c6a61a232
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-02-18 15:08:52 +0200

    basic setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,26 @@
+import { type AppType } from "next/app";
+import { type Session } from "next-auth";
+import { SessionProvider } from "next-auth/react";
+import { api } from "../utils/api";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "@fontsource/inter"
+const theme = extendTheme({
+  fonts: {
+    heading: "Inter",
+    body: "Inter",
+  },
+})
+const MyApp: AppType<{ session: Session | null }> = ({
+  Component,
+  pageProps: { session, ...pageProps },
+}) => {
+  return (
+    <SessionProvider session={session}>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </SessionProvider>
+  );
+};
+
+export default api.withTRPC(MyApp);
