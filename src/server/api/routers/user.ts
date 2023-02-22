88ed274525f538180ddfe9f185bc189a3af7f7df
Youssef Amr El-Shehaby <youssef.amr90@gmail.com>
2023-02-22 20:13:12 +0200

    create and delete are redundant

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -9,36 +9,5 @@ export const userRouter = createTRPCRouter({
       },
     });
   }),
-  deleteUser: publicProcedure.input(z.string()).query(({ ctx, input }) => {
-    return ctx.prisma.user.delete({
-      where: {
-        id: input,
-      },
-    });
-  }),
-  login: publicProcedure
-    .input(z.object({ email: z.string(), password: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.prisma.user.findFirst({
-        where: {
-          id: ctx.session?.user.id,
-          email: input.email,
-        },
-      });
-
-      if (!user) {
-        throw new Error("User not found");
-      }
-      return user;
-    }),
-  createUser: publicProcedure
-    .input(z.object({ email: z.string(), password: z.string() }))
-    .query(async ({ ctx, input }) => {
-      const user = await ctx.prisma.user.create({
-        data: {
-          email: input.email,
-        },
-      });
-      return user;
-    }),
+  
 });
